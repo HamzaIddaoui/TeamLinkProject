df41613880422a22d9edc21690a85b3072023ef5
Hamza Iddaoui <hamzaiddaoui99@gmail.com>
2024-01-01 18:40:55 +0100

    Step 1 commit

---
+++ TeamLinkFront/components/AddEventForm.js
@@ -0,0 +1,17 @@
+import { Card, Text, View } from '@ant-design/react-native';
+import React, { useState } from 'react';
+
+
+const AddEventForm = () => {
+    return (
+        <View style={{flex : 1}}>
+            <Card>
+            <View>
+                <Text>Heeyhhh</Text>
+            </View>
+            </Card>
+        </View>
+    )
+}
+
+export default AddEventForm;
